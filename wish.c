7d4bde90e5ceec88a57f944ccd8c26765bd64ea8
flavius-t <flavius.t@hotmail.com>
2023-05-21 01:44:19 -0700

    add input tokenization

--- wish.c
+++ wish.c
@@ -5,6 +5,8 @@
 #include <string.h>
 #include <ctype.h>
 
+#define MAX_TOKENS 64
+
 
 int exit_check(char* line) {
     if (line[0] == 'e' && line[1] == 'x' && line[2] == 'i' && line[3] == 't') {
@@ -15,7 +17,7 @@ int exit_check(char* line) {
 }
 
 
-void execute_cmd(char* cmd) {
+void execute_input(char** tokens) {
     int rc = fork();
     if (rc < 0) {
         fprintf(stderr, "fork failed\n");
@@ -23,11 +25,10 @@ void execute_cmd(char* cmd) {
     } else if (rc == 0) {
         printf("hello, I am child (pid:%d)\n", (int) getpid());
 
-        // TODO: temporarily hardcode command and args; need to parse input str into command and separate args tokens
-        char* path = "/usr/bin/ls";
-        char* argv[] = { path, cmd, NULL };
+        execv(tokens[0], tokens);
 
-        execv(path, argv);
+        fprintf(stderr, "failed to execute command\n");
+        exit(1);
 
     } else {
         int wc = wait(NULL);
@@ -52,6 +53,50 @@ void strip_whitespace(char* str) {
 }
 
 
+/*
+    * Tokenize user input, for use with execv.
+    * Returns an array of tokens, where the first token is the command and the rest are args.
+    * Returns NULL if malloc fails.
+*/
+char** tokenize(char* str) {
+    char** tokens = (char**)malloc(MAX_TOKENS * sizeof(char*));
+
+    if (!tokens) {
+        fprintf(stderr, "tokenize -- malloc failed\n");
+        return NULL;
+    }
+
+    char* token = NULL;
+    size_t i = 0;
+
+    while ((token = strtok(str, " ")) != NULL) {
+        size_t tokenLength = strlen(token);
+        tokens[i] = (char*)malloc((tokenLength + 1) * sizeof(char));
+        strcpy(tokens[i], token);
+        i++;
+        str = NULL;
+    }
+
+    tokens[i] = NULL; // indicates end of array for looping
+
+    token = NULL;
+    return tokens;
+
+}
+
+
+void free_tokens(char** tokens) {
+    if (!tokens) {
+        return;
+    }
+
+    for (size_t i = 0; tokens[i] != NULL; i++) {
+        free(tokens[i]);
+    }
+    free(tokens);
+}
+
+
 int main(int argc, char* argv[]) {
     // check if interactive or batch mode
     if (argc == 1) {
@@ -61,10 +106,8 @@ int main(int argc, char* argv[]) {
         while (1) {
             printf("wish> ");
 
-            // read line
             getline(&line, &line_size, stdin);
 
-            // parse line: clean and process args
             strip_whitespace(line);
 
             if (exit_check(line)) {
@@ -73,7 +116,11 @@ int main(int argc, char* argv[]) {
                 break;
             }
 
-            execute_cmd(line);
+            char** tokens = tokenize(line);
+
+            execute_input(tokens);
+
+            free_tokens(tokens);
         }
     } else if (argc == 2) {
         printf("Running batch mode with script: %s\n", argv[1]);
